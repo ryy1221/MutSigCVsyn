6971f0b0f64da6b92fc1613c772e659c225919c8
Yiyun Rao <yur97@psu.edu>
2020-08-13 00:16:45 -0400

    modify merge coverage code

--- cov_process/merge_coverage.py
+++ cov_process/merge_coverage.py
@@ -1,13 +1,29 @@
 #This script is for merging the coverage file of patient to generate a consensus coverage file
 
-import os
 from os import listdir
 from os.path import isfile, join
-import re
-import time
-import shutil
-import os.path
+from optparse import OptionParser
 import csv
+
+def parse_arguments():
+    parser = OptionParser("Usage: %prog -i <inputpath>")
+    parser.add_option("-i", "--info_file",
+                      dest="inputPath",
+                      help="The full absolute path of the PCAWG ICGC sample info file")
+
+    parser.add_option("-d", "--cov_dir",
+                     dest="inputPath1",
+                     help="The full absolute path of the coverage directory")
+
+    parser.add_option("-o", "--output_path",
+                      dest="inputPath1",
+                      help="The full absolute path of the output folder")
+
+    (options, arg) = parser.parse_args()
+    if not options.inputPath:
+        parser.error("Requires at least one input file, the path of this script. Run with --help for help.")
+        quit(1)
+    return options
 
 # get all patient information
 # this function parse the patient information file
@@ -19,23 +35,22 @@ def parse_info_file(info_file):
             line_split = lines.split('\t')
             tumor_aliquot_id, normal_aliquot_id, donor_id, sample_id, specimen_id, project_code, gender, age \
                 = line_split[0:8]
+            histology_abbreviation = line_split[12]
             if donor_id not in donor_dict:
                 donor_dict[donor_id] = []
             donor_dict[donor_id] = tumor_aliquot_id
 
     return donor_dict
 
-dict_donor = parse_info_file('/gpfs/scratch/yur97/coverage_run/PCAWG_sample_info.txt')
+# get patient list in organ
+def get_patient_list(coverage_organ_dir):
+    list_file = listdir(coverage_organ_dir)
+    list_patient_id = []
+    for files in list_file:
+        patient_id = files.split('.')[0]
+        list_patient_id.append(patient_id)
 
-coverage_dir = '/gpfs/scratch/yur97/merge_coverage_test/coverage_files/'
-list_file = listdir(coverage_dir)
-print list_file
-
-list_patient_id = []
-for files in list_file:
-    patient_id = files.split('.')[0]
-    list_patient_id.append(patient_id)
-print list_patient_id
+    return list_patient_id
 
 
 # this function create a dictionary to store patient coverage values
@@ -70,25 +85,43 @@ def create_coverage_dict(patient_file, dict_coverage):
     return dict_coverage
 
 
-coverage_dir = '/gpfs/scratch/yur97/merge_coverage_test/coverage_files/'
-list_file = listdir(coverage_dir)
-coverage_dict = {}
-for f in list_file:
-    absolute_f = join(coverage_dir, f)
-    coverage_dict = create_coverage_dict(absolute_f, coverage_dict)
+if __name__ == '__main__':
+    arguments = parse_arguments()
+    info_f = arguments.inputPath
+    cov_dir = arguments.inputPath1
+    out_p = arguments.inputPath2
+
+    dict_donor = parse_info_file(info_f)
+    list_dirs = listdir(cov_dir)
+    for dirs in list_dirs:
+        organ_folder_path = join(cov_dir, dirs)
+        organ_type = dirs
+        # get patient list for this organ
+        patient_list = get_patient_list(organ_folder_path)
 
 
-with open('/gpfs/scratch/yur97/merge_coverage_test/coverage_merged.txt','wb') as merge_f:
-    merge_f.write('gene'+'\t'+'zone'+'\t'+'categ')
-    for p in list_patient_id:
-        merge_f.write('\t'+p)
-        if p == list_patient_id[-1]:
-            merge_f.write('\n')
-    for genes in coverage_dict:
-        for zones in coverage_dict[genes]:
-            for i in coverage_dict[genes][zones]:
-                merge_f.write(genes+'\t' + zones +'\t' + str(i))
-                for patients in coverage_dict[genes][zones][i]:
-                    merge_f.write('\t'+str(coverage_dict[genes][zones][i][patients]))
-                    if patients == coverage_dict[genes][zones][i].keys()[-1]:
-                        merge_f.write('\n')
+        # get file list in this organ type
+        organ_f_list = listdir(organ_folder_path)
+        coverage_dict = {}
+        for f in organ_f_list:
+            p_f = join(organ_folder_path, f)
+            coverage_dict = create_coverage_dict(p_f, coverage_dict)
+
+
+        # creat output file
+        with open(out_p+'merged'+organ_type+'.txt', 'wb') as merge_f:
+            merge_f.write('gene'+'\t'+'zone'+'\t'+'categ')
+            for p in patient_list:
+                merge_f.write('\t'+p)
+                if p == patient_list[-1]:
+                    merge_f.write('\n')
+            for genes in coverage_dict:
+                for zones in coverage_dict[genes]:
+                    for i in coverage_dict[genes][zones]:
+                        merge_f.write(genes+'\t' + zones +'\t' + str(i))
+                        for patients in coverage_dict[genes][zones][i]:
+                            merge_f.write('\t'+str(coverage_dict[genes][zones][i][patients]))
+                            if patients == coverage_dict[genes][zones][i].keys()[-1]:
+                                merge_f.write('\n')
+
+
